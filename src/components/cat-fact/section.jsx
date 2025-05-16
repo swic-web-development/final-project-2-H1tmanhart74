a08606337f4b265fb18eb617ddf399b28d381ce4
John Hart <john.hart003@student.swic.edu>
2025-05-16 05:27:06 -0500

    add stylistic elements

--- src/components/cat-fact/section.jsx
+++ src/components/cat-fact/section.jsx
@@ -1,10 +1,10 @@
 export default function Section({ onClick, newFact, fact }) {
   return (
-    <section className="flex flex-col items-center justify-center rounded-lg bg-gray-900 p-4">
+    <section className="flex flex-col items-center justify-center rounded-lg bg-gray-900 p-4 shadow-lg">
       <h2 className="text-xl font-bold text-blue-400">Fact</h2>
-      <p className="text-blue-400">{fact}</p>
+      <p className="py-2 text-blue-400">{fact}</p>
       <button
-        className="rounded bg-blue-500 px-4 py-2 text-white transition hover:bg-blue-600"
+        className="rounded bg-gray-800 px-4 py-1 text-blue-400 transition hover:bg-blue-800"
         onClick={onClick}
       >
         {newFact}
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <h1 className="bg-clip-text py-2 text-2xl font-bold text-blue-300 uppercase">
+      <h1 className="bg-clip-text py-1 text-2xl font-bold text-blue-300 uppercase">
         Random Cat Facts
       </h1>
     </header>
